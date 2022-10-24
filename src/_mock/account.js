df5c2524dd326f4c1a7066c09e06c770636995ba
shafi_007 <shafiptb07@gmail.com>
2022-10-24 09:37:36 +0530

    initial commit

---
+++ src/_mock/account.js
@@ -0,0 +1,10 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'ALINMA TRAVELS',
+  email: 'admin@alinma.com',
+  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
+  role:'admin'
+};
+
+export default account;
---
+++ src/sections/@dashboard/user/Filter.js
@@ -0,0 +1,40 @@
+import { useRef, useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+// material
+import { Menu, MenuItem, IconButton, ListItemIcon, ListItemText } from '@mui/material';
+// component
+import Iconify from '../../../components/Iconify';
+
+// ----------------------------------------------------------------------
+
+export default function Filter() {
+  const ref = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <IconButton ref={ref} onClick={() => setIsOpen(true)}>
+        <Iconify icon="ic:round-filter-list" width={20} height={20} />
+      </IconButton>
+
+      <Menu
+        open={isOpen}
+        anchorEl={ref.current}
+        onClose={() => setIsOpen(false)}
+        PaperProps={{
+          sx: { width: 200, maxWidth: '100%' },
+        }}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <MenuItem sx={{ color: 'red' }}>
+          <ListItemText primary="Debit" primaryTypographyProps={{ variant: 'body2' }} />
+        </MenuItem>
+
+        <MenuItem component={RouterLink} to="#" sx={{ color: 'green' }}>
+          <ListItemText primary="Paid" color='error' primaryTypographyProps={{ variant: 'body2' }} />
+        </MenuItem>
+      </Menu>
+    </>
+  );
+}
