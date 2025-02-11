daacf7e905961c82c638a25c2d673d2bb1cfdb8b
shafi_007 <shafiptb07@gmail.com>
2025-02-11 11:14:15 +0530

    Added view in zakath

--- src/pages/zakath.js
+++ src/pages/zakath.js
@@ -133,11 +133,28 @@ export default function Zakath() {
           api={params.api}
           onDelete={handleDeleteRow}
           onPrint = {handlePrint}
-          print = {true}
         />
       ), 
       // minWidth: 194,
-      width: 130,
+      width: 80,
+      floatingFilter: false ,
+      filter: false  
+    },
+    { 
+      field: "View/Print",
+      cellRenderer: (params) => (
+        <DeleteCellRenderer
+          node={params.node}
+          api={params.api}
+          onDelete={handleDeleteRow}
+          onPrint = {handlePrint}
+          onView = {viewOpen}
+          print = {true}
+          view = {true}
+        />
+      ), 
+      // minWidth: 194,
+      width: 140,
       floatingFilter: false ,
       filter: false  
     },
